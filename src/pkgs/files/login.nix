ac42d7e6bb3b83048429ac06d4a77e7771f345e7
Tobias Happ <tobias.happ@gmx.de>
2019-09-21 13:02:25 +0200

    Add license header in each file

--- src/pkgs/files/login.nix
+++ src/pkgs/files/login.nix
@@ -1,3 +1,6 @@
+# Licensed under GNU Lesser General Public License v3 or later, see COPYING.
+# Copyright (c) 2019 Alexander Sosedkin and other contributors, see AUTHORS.
+
 { instDir, writeScript }:
 
 writeScript "login" ''
