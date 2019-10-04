6d3e430a3a8e5b688ba07e02dad175687b7dd251
Tobias Happ <tobias.happ@gmx.de>
2019-10-04 17:58:23 +0200

    Add missing parameter

--- src/pkgs/default.nix
+++ src/pkgs/default.nix
@@ -1,7 +1,7 @@
 # Licensed under GNU Lesser General Public License v3 or later, see COPYING.
 # Copyright (c) 2019 Alexander Sosedkin and other contributors, see AUTHORS.
 
-{ arch, buildPkgs, crossPkgs, crossStaticPkgs, pinnedPkgs } @ args:
+{ arch, buildPkgs, crossPkgs, crossStaticPkgs, initialBuild, pinnedPkgs } @ args:
 
 let
   callPackage = buildPkgs.lib.callPackageWith (args // pkgs);
