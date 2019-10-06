dfa9686aa60873966bb7a96dc22fa283000f6e94
Alexander Sosedkin <monk@unboiled.info>
2019-10-06 04:41:06 +0200

    Add the missing field in /etc/passwd user entry

--- src/pkgs/files/etc-passwd.nix
+++ src/pkgs/files/etc-passwd.nix
@@ -9,5 +9,5 @@ in
 
 writeTextDir "etc/passwd" ''
   root:x:0:0:System administrator:${instDir}/root:/bin/sh
-  ${userName}:x:${idSet.uid}:${idSet.gid}:/data/data/com.termux.nix/files/home:${shell}
+  ${userName}:x:${idSet.uid}:${idSet.gid}:${userName}:/data/data/com.termux.nix/files/home:${shell}
 ''
