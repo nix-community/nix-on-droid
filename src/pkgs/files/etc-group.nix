f133fcf04b8b8212e717daf15d7c7695594d17ae
Tobias Happ <tobias.happ@gmx.de>
2019-11-24 00:38:50 +0100

    Remove deprecated src directory

--- src/pkgs/files/etc-group.nix
+++
@@ -1,9 +0,0 @@
-# Licensed under GNU Lesser General Public License v3 or later, see COPYING.
-# Copyright (c) 2019 Alexander Sosedkin and other contributors, see AUTHORS.
-
-{ config, writeTextDir }:
-
-writeTextDir "etc/group" ''
-  root:x:0:
-  ${config.user.group}:x:${config.user.gid}:${config.user.user}
-''
--- src/pkgs/files/etc-passwd.nix
+++
@@ -1,9 +0,0 @@
-# Licensed under GNU Lesser General Public License v3 or later, see COPYING.
-# Copyright (c) 2019 Alexander Sosedkin and other contributors, see AUTHORS.
-
-{ config, writeTextDir }:
-
-writeTextDir "etc/passwd" ''
-  root:x:0:0:System administrator:${config.core.installation}/root:/bin/sh
-  ${config.user.user}:x:${config.user.uid}:${config.user.gid}:${config.user.user}:${config.user.home}:${config.user.shell}
-''
