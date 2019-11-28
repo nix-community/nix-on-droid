93ef4cf539f210dffd2a3df577c70bf12c8b12d5
Tobias Happ <tobias.happ@gmx.de>
2019-11-28 17:18:48 +0100

    Update nixpkgs revision

--- pkgs/lib/load-nixpkgs.nix
+++ pkgs/lib/load-nixpkgs.nix
@@ -7,11 +7,11 @@ let
     overlays = [ ];
   };
 
-  # head of nixos-19.09 as of 2019-10-14
+  # head of nixos-19.09 as of 2019-11-28
   # note: when updating nixpkgs, update store paths of proot-termux in modules/environment/login/default.nix
   pinnedPkgsSrc = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/45a25ff41a2309099ff2e70b2f37debd5e567618.tar.gz";
-    sha256 = "0w37p98i47d7snhckz8a3a60v0f05b5047vsm4rczink9mk9x7r3";
+    url = "https://github.com/NixOS/nixpkgs/archive/73fb59dbb89ed5f754249761dcd99c6ccbd24bb5.tar.gz";
+    sha256 = "0fp85c907qw1qnxs40dx4yas9z5fqr9gszk4nazx90hwbimyk6n6";
   };
 in
 
