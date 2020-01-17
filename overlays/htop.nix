7f2331bb02044a3e7bb93c9101b4922490453e43
Alexander Sosedkin <monk@unboiled.info>
2020-01-17 03:45:49 +0100

    Pin htop patch revision

--- overlays/htop.nix
+++ overlays/htop.nix
@@ -10,7 +10,7 @@ in
   htop = nixpkgs.htop.overrideAttrs (old: {
     patches = [
       (super.fetchpatch {
-        url = "https://raw.githubusercontent.com/termux/termux-packages/master/packages/htop/fix-missing-macros.patch";
+        url = "https://raw.githubusercontent.com/termux/termux-packages/04aea16b13a246c478d28b8cc8c552a052f225ea/packages/htop/fix-missing-macros.patch";
         sha256 = "1cljkjagp66xxcjb6y1m9k4v994slfkd0s6fijh02l3rp8ycvjnv";
       })
     ];
