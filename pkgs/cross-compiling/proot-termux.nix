6d7edfc20bd0817fb400d8fabce81993340fb153
Alexander Sosedkin <monk@unboiled.info>
2021-06-19 05:02:46 +0200

    pkgs: update talloc and proot

--- pkgs/cross-compiling/proot-termux.nix
+++ pkgs/cross-compiling/proot-termux.nix
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, see AUTHORS. Licensed under MIT License, see LICENSE.
+# Copyright (c) 2019-2021, see AUTHORS. Licensed under MIT License, see LICENSE.
 
 { callPackage, fetchFromGitHub, tallocStatic }:
 
@@ -8,20 +8,25 @@ in
 
 pkgs.crossStatic.stdenv.mkDerivation {
   pname = "proot-termux";
-  version = "unstable-2020-10-25";
+  version = "unstable-2021-05-19";
 
   src = fetchFromGitHub {
     repo = "proot";
     owner = "termux";
-    rev = "66b34c6fb38983b09da3400b8bcf86005ebe8dd1";
-    sha256 = "0isrjcblkdkikw6l6f7a2p326vsy3plbs9ga48r20lpa8rsz4jnf";
+    rev = "3b7369b8eb8b2a879aade2b403b3ac0eb848b9ed";  # the one with make fix
+    sha256 = "1wrrar08axfwrma7yp2zlf61cz4crypr3m1jnhkqng7p1pry1cay";
+
+    # 1 step behind 6f12fbee "Implement shmat", use if ashmem.h is missing
+    #rev = "ffd811ee726c62094477ed335de89fc107cadf17";
+    #sha256 = "1zjblclsybvsrjmq2i0z6prhka268f0609w08dh9vdrbrng117f8";
+
   };
 
   buildInputs = [ tallocStatic ];
 
   patches = [ ./proot-detranslate-empty.patch ];
 
-  makeFlags = [ "-Csrc CFLAGS=-D__ANDROID__" ];
+  makeFlags = [ "-Csrc" "V=1" ];
 
   installPhase = ''
     install -D -m 0755 src/proot $out/bin/proot-static
