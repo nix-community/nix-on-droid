3562ffaf44561d2b35da688aca4dc1b0347019ab
Alexander Sosedkin <monk@unboiled.info>
2021-06-24 21:53:24 +0200

    pkgs/qemu-aarch64-static: update to 5.2.0-2

--- pkgs/qemu-aarch64-static.nix
+++ pkgs/qemu-aarch64-static.nix
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, see AUTHORS. Licensed under MIT License, see LICENSE.
+# Copyright (c) 2019-2021, see AUTHORS. Licensed under MIT License, see LICENSE.
 
 { stdenv }:
 
@@ -6,8 +6,8 @@ stdenv.mkDerivation {
   name = "qemu-aarch64-static";
 
   src = builtins.fetchurl {
-    url = "https://github.com/multiarch/qemu-user-static/releases/download/v5.1.0-7/qemu-aarch64-static";
-    sha256 = "0yzlrlknslvas58msrbbq3hazphyydrbaqrd840bd1c7vc9lcrh6";
+    url = "https://github.com/multiarch/qemu-user-static/releases/download/v5.2.0-2/qemu-aarch64-static";
+    sha256 = "0v1c8nchf5s7db11spixp2gsp94018ig7nz2ha1f4bngr0bgbk92";
   };
 
   dontUnpack = true;
