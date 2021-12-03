f7c4ecd7f4772afecaa491960fb8511ce6ae6884
Zhaofeng Li <hello@zhaofeng.li>
2021-12-03 09:32:29 -0800

    modules: Accept a path-like for home-manager
    We don't actually use the initialized attrset for anything
    other than its path.

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -7,13 +7,13 @@ with lib;
 let
   cfg = config.home-manager;
 
-  extendedLib = import (home-manager.path + "/modules/lib/stdlib-extended.nix") pkgs.lib;
+  extendedLib = import (home-manager + "/modules/lib/stdlib-extended.nix") pkgs.lib;
 
   hmModule = types.submoduleWith {
     specialArgs = { lib = extendedLib; };
     modules = [
       ({ name, ... }: {
-        imports = import (home-manager.path + "/modules/modules.nix") {
+        imports = import (home-manager + "/modules/modules.nix") {
           inherit pkgs;
           lib = extendedLib;
           useNixpkgsModule = !cfg.useGlobalPkgs;
