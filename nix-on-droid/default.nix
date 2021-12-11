3d3441de44e51ae864987c3d2960ed5850ffe299
Tobias Happ <tobias.happ@gmx.de>
2021-12-12 00:20:48 +0100

    nix-on-droid: add native support for flakes

--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -1,6 +1,6 @@
 # Copyright (c) 2019-2020, see AUTHORS. Licensed under MIT License, see LICENSE.
 
-{ bash, coreutils, nix, runCommand }:
+{ bash, coreutils, nix, nix_2_4, runCommand }:
 
 runCommand
   "nix-on-droid"
@@ -14,5 +14,6 @@ runCommand
     substituteInPlace $out/bin/nix-on-droid \
       --subst-var-by bash "${bash}" \
       --subst-var-by coreutils "${coreutils}" \
-      --subst-var-by nix "${nix}"
+      --subst-var-by nix "${nix}" \
+      --subst-var-by nix24 "${nix_2_4}"
   ''
