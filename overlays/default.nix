c1b61fc3404a2a60392b48fce1f36029349a1bfe
Alexander Sosedkin <monk@unboiled.info>
2021-12-21 21:44:19 +0100

    overlays/htop: remove since /proc/stat faking is enough

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,5 @@
-# Copyright (c) 2019-2020, see AUTHORS. Licensed under MIT License, see LICENSE.
+# Copyright (c) 2019-2021, see AUTHORS. Licensed under MIT License, see LICENSE.
 
 [
-  (import ./htop.nix)
   (import ./typespeed.nix)
 ]
