b10dd78e183f76dbb15711475fa7643b55046757
Tobias Happ <tobias.happ@gmx.de>
2022-09-24 13:43:49 +0200

    bootstrap: refactor special args to reduce inputs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,10 @@
-# Copyright (c) 2019-2021, see AUTHORS. Licensed under MIT License, see LICENSE.
+# Copyright (c) 2019-2022, see AUTHORS. Licensed under MIT License, see LICENSE.
 
 { arch ? "aarch64", nixOnDroidChannelURL ? null, nixpkgsChannelURL ? null }:
 
 let
   nixDirectory = callPackage ./nix-directory.nix { };
-  packageInfo = import "${nixDirectory}/nix-support/package-info.nix";
+  initialPackageInfo = import "${nixDirectory}/nix-support/package-info.nix";
 
   nixpkgs = import lib/load-nixpkgs.nix { };
 
@@ -13,13 +13,13 @@ let
 
     extraModules = [ ../modules/build/initial-build.nix ];
     extraSpecialArgs = {
-      inherit customPkgs;
+      inherit initialPackageInfo;
       pkgs = nixpkgs.lib.mkForce nixpkgs; # to override ./modules/nixpkgs/config.nix
     };
 
     config = {
       # Fix invoking bash after initial build.
-      user.shell = "${packageInfo.bash}/bin/bash";
+      user.shell = "${initialPackageInfo.bash}/bin/bash";
 
       build = {
         inherit arch;
@@ -35,12 +35,11 @@ let
   callPackage = nixpkgs.lib.callPackageWith (
     nixpkgs // customPkgs // {
       inherit (modules) config;
-      inherit callPackage;
+      inherit callPackage nixpkgs nixDirectory initialPackageInfo;
     }
   );
 
   customPkgs = {
-    inherit nixDirectory packageInfo;
     bootstrap = callPackage ./bootstrap.nix { };
     bootstrapZip = callPackage ./bootstrap-zip.nix { };
     prootTermux = callPackage ./cross-compiling/proot-termux.nix { };
