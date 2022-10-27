1dc1cd67a47890c4b209fd16c181280788b0897f
Tobias Happ <tobias.happ@gmx.de>
2022-10-27 20:29:34 +0200

    bootstrap: override flake inputs if needed

--- modules/build/initial-build.nix
+++ modules/build/initial-build.nix
@@ -4,6 +4,17 @@
 
 with lib;
 
+let
+  defaultNixpkgsBranch = "nixos-22.05";
+  defaultNixOnDroidBranch = "release-22.05";
+
+  defaultNixpkgsChannel = "https://nixos.org/channels/${defaultNixpkgsBranch}";
+  defaultNixOnDroidChannel = "https://github.com/t184256/nix-on-droid/archive/${defaultNixOnDroidBranch}.tar.gz";
+
+  defaultNixpkgsFlake = "github:NixOS/nixpkgs/${defaultNixpkgsBranch}";
+  defaultNixOnDroidFlake = "github:t184256/nix-on-droid/${defaultNixOnDroidBranch}";
+in
+
 {
 
   ###### interface
@@ -14,22 +25,34 @@ with lib;
       channel = {
         nixpkgs = mkOption {
           type = types.str;
-          default = "https://nixos.org/channels/nixos-22.05";
+          default = defaultNixpkgsChannel;
           description = "Channel URL for nixpkgs.";
         };
 
         nix-on-droid = mkOption {
           type = types.str;
-          default = "https://github.com/t184256/nix-on-droid/archive/release-22.05.tar.gz";
+          default = defaultNixOnDroidChannel;
           description = "Channel URL for nix-on-droid.";
         };
       };
 
       flake = {
+        nixpkgs = mkOption {
+          type = types.str;
+          default = defaultNixpkgsFlake;
+          description = "Flake URL for nixpkgs.";
+        };
+
         nix-on-droid = mkOption {
           type = types.str;
-          default = "github:t184256/nix-on-droid/release-22.05";
+          default = defaultNixOnDroidFlake;
           description = "Flake URL for nix-on-droid.";
+        };
+
+        inputOverrides = mkEnableOption "" // {
+          description = ''
+            Whether to override the standard input URLs in the initial <filename>flake.nix</filename>.
+          '';
         };
       };
     };
@@ -41,7 +64,13 @@ with lib;
 
   config = {
 
-    build.initialBuild = true;
+    build = {
+      initialBuild = true;
+
+      flake.inputOverrides =
+        config.build.flake.nixpkgs != defaultNixpkgsFlake
+        || config.build.flake.nix-on-droid != defaultNixOnDroidFlake;
+    };
 
     # /etc/group and /etc/passwd need to be build on target machine because
     # uid and gid need to be determined.
