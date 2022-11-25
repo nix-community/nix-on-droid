dfc9250a8e223a1928460b89e93e33694e2278d4
Pavel Atanasov <apavel@vmware.com>
2022-11-25 20:01:43 +0200

    fix(terminal.nix): quote font paths

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -44,24 +44,24 @@ in
       if (cfg.font != null) then
         {
           linkFont = ''
-            $DRY_RUN_CMD mkdir $VERBOSE_ARG -p ${configDir}
-            if [ -e ${fontTarget} ] && ! [ -L ${fontTarget} ]; then
-              $DRY_RUN_CMD mv $VERBOSE_ARG ${fontTarget} ${fontBackup}
+            $DRY_RUN_CMD mkdir $VERBOSE_ARG -p "${configDir}"
+            if [ -e "${fontTarget}" ] && ! [ -L "${fontTarget}" ]; then
+              $DRY_RUN_CMD mv $VERBOSE_ARG "${fontTarget}" "${fontBackup}"
               $DRY_RUN_CMD echo "${fontTarget} has been moved to ${fontBackup}"
             fi
-            $DRY_RUN_CMD ln $VERBOSE_ARG -sf ${fontPath} ${fontTarget}
+            $DRY_RUN_CMD ln $VERBOSE_ARG -sf "${fontPath}" "${fontTarget}"
           '';
         }
       else
         {
           unlinkFont = ''
-            if [ -e ${fontTarget} ] && [ -L ${fontTarget} ]; then
-              $DRY_RUN_CMD rm $VERBOSE_ARG ${fontTarget}
-              if [ -e ${fontBackup} ]; then
-                $DRY_RUN_CMD mv $VERBOSE_ARG ${fontBackup} ${fontTarget}
+            if [ -e "${fontTarget}" ] && [ -L "${fontTarget}" ]; then
+              $DRY_RUN_CMD rm $VERBOSE_ARG "${fontTarget}"
+              if [ -e "${fontBackup}" ]; then
+                $DRY_RUN_CMD mv $VERBOSE_ARG "${fontBackup}" "${fontTarget}"
                 $DRY_RUN_CMD echo "${fontTarget} has been restored from backup"
               else
-                if $DRY_RUN_CMD rm $VERBOSE_ARG -d ${configDir} 2>/dev/null
+                if $DRY_RUN_CMD rm $VERBOSE_ARG -d "${configDir}" 2>/dev/null
                 then
                   $DRY_RUN_CMD echo "removed empty ${configDir}"
                 fi
