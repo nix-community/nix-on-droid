3dfb9d44b545ab815ca4a4ca72d2aa14065dce33
Tobias Happ <tobias.happ@gmx.de>
2022-12-01 22:47:04 +0100

    bootstrap: update nixpkgs version

--- modules/environment/login/default.nix
+++ modules/environment/login/default.nix
@@ -82,7 +82,7 @@ in
     environment.files = {
       inherit login loginInner;
 
-      prootStatic = "/nix/store/wsgnxpmrdmjy7qrsxvp7r5jandbabzjl-proot-termux-static-aarch64-unknown-linux-android-unstable-2022-05-03";
+      prootStatic = "/nix/store/8xg8jnaczivjjrd9nq38qvrp9z7avib1-proot-termux-static-aarch64-unknown-linux-android-unstable-2022-05-03";
     };
 
   };
