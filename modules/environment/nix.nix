23b8cce084ff47723f2c806b3e6b0eac32dd89bf
Yueh-Shun Li <shamrocklee@posteo.net>
2024-02-16 01:15:04 +0800

    nix-flakes.nix, nix-channel.nix: split out from nix.nix
    Decouple the modules, enable ones to import/disable them separately, and
    to align with the upstream Nixpkgs/NixOS module system.

--- modules/environment/nix.nix
+++ modules/environment/nix.nix
@@ -1,7 +1,9 @@
 # Copyright (c) 2019-2022, see AUTHORS. Licensed under MIT License, see LICENSE.
 
 # Based on
-# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/misc/nix-daemon.nix
+# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/system/nix-daemon.nix
+# and
+# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/config/nix.nix
 # (Copyright (c) 2003-2022 Eelco Dolstra and the Nixpkgs/NixOS contributors,
 # licensed under MIT License as well)
 
@@ -27,6 +29,9 @@ in
   options = {
 
     nix = {
+
+      ## From nix-daemon.nix
+
       enable = mkOption {
         type = types.bool;
         default = true;
@@ -46,72 +51,7 @@ in
         '';
       };
 
-      nixPath = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        description = ''
-          The default Nix expression search path, used by the Nix
-          evaluator to look up paths enclosed in angle brackets
-          (e.g. <literal>&lt;nixpkgs&gt;</literal>).
-        '';
-      };
-
-      registry = mkOption {
-        type = types.attrsOf (types.submodule (
-          let
-            referenceAttrs = with types; attrsOf (oneOf [
-              str
-              int
-              bool
-              package
-            ]);
-          in
-          { config, name, ... }:
-          {
-            options = {
-              from = mkOption {
-                type = referenceAttrs;
-                example = { type = "indirect"; id = "nixpkgs"; };
-                description = "The flake reference to be rewritten.";
-              };
-              to = mkOption {
-                type = referenceAttrs;
-                example = { type = "github"; owner = "my-org"; repo = "my-nixpkgs"; };
-                description = "The flake reference <option>from</option> is rewritten to.";
-              };
-              flake = mkOption {
-                type = types.nullOr types.attrs;
-                default = null;
-                example = literalExpression "nixpkgs";
-                description = ''
-                  The flake input <option>from</option> is rewritten to.
-                '';
-              };
-              exact = mkOption {
-                type = types.bool;
-                default = true;
-                description = ''
-                  Whether the <option>from</option> reference needs to match exactly. If set,
-                  a <option>from</option> reference like <literal>nixpkgs</literal> does not
-                  match with a reference like <literal>nixpkgs/nixos-20.03</literal>.
-                '';
-              };
-            };
-            config = {
-              from = mkDefault { type = "indirect"; id = name; };
-              to = mkIf (config.flake != null) (mkDefault
-                {
-                  type = "path";
-                  path = config.flake.outPath;
-                } // filterAttrs
-                (n: _: n == "lastModified" || n == "rev" || n == "revCount" || n == "narHash")
-                config.flake);
-            };
-          }
-        ));
-        default = { };
-        description = "A system-wide flake registry.";
-      };
+      ## From nix.nix
 
       substituters = mkOption {
         type = types.listOf types.str;
@@ -152,11 +92,6 @@ in
         trusted-public-keys = ${concatStringsSep " " cfg.trustedPublicKeys}
         ${cfg.extraOptions}
       '';
-
-      "nix/registry.json".text = builtins.toJSON {
-        version = 2;
-        flakes = mapAttrsToList (_n: v: { inherit (v) from to exact; }) cfg.registry;
-      };
     };
 
     nix = {
@@ -170,8 +105,6 @@ in
         "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
       ];
     };
-
-    environment.sessionVariables.NIX_PATH = concatStringsSep ":" cfg.nixPath;
   };
 
 }
