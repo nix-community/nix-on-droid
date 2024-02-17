970a735f567e5a7859ad5433eadd7f2cae44e812
Alexander Sosedkin <monk@unboiled.info>
2024-02-17 13:24:08 +0100

    pkgs/nix-directory: update nix to 2.20.4
    Co-authored-by: Tobias Happ <tobias.happ@gmx.de>

--- pkgs/bootstrap.nix
+++ pkgs/bootstrap.nix
@@ -1,9 +1,10 @@
-# Copyright (c) 2019-2022, see AUTHORS. Licensed under MIT License, see LICENSE.
+# Copyright (c) 2019-2024, see AUTHORS. Licensed under MIT License, see LICENSE.
 
 { runCommand, nixDirectory, prootTermux, bash, pkgs, config, initialPackageInfo }:
 
 runCommand "bootstrap" { } ''
-  mkdir --parents $out/{.l2s,bin,dev/shm,etc,nix,root,tmp,usr/{bin,lib}}
+  mkdir --parents $out/{.l2s,bin,dev/shm,etc,root,tmp,usr/{bin,lib}}
+  mkdir --parents $out/nix/var/nix/{profiles,gcroots}/per-user/nix-on-droid
 
   cp --recursive ${nixDirectory}/store $out/nix/store
   cp --recursive ${nixDirectory}/var $out/nix/var
