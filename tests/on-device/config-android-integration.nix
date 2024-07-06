b195fa0b15e6b5d1aa3a0512d7da841f2e688bea
Alexander Sosedkin <monk@unboiled.info>
2024-07-06 17:10:49 +0200

    modules/android-integration: termux-open, termux-open-url, xdg-open

--- tests/on-device/config-android-integration.nix
+++ tests/on-device/config-android-integration.nix
@@ -4,7 +4,10 @@ _:
   system.stateVersion = "23.11";
   android-integration = {
     am.enable = true;
+    termux-open.enable = true;
+    termux-open-url.enable = true;
     termux-setup-storage.enable = true;
+    xdg-open.enable = true;
     # unsupported.enable = false;
   };
 }
