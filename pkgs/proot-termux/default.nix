79a6a24d7de0f978deee021a129e6e5b2873362d
Peter Marshall <petms@proton.me>
2026-02-17 16:56:19 -0500

    proot: Update to fix crash when linked with musl libc
    See https://github.com/termux/proot/pull/335

--- pkgs/proot-termux/default.nix
+++ pkgs/proot-termux/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation {
   pname = "proot-termux";
-  version = "unstable-2024-05-04";
+  version = "28baec5ac3e0f26c03cf2a4cdc094c9328bd5b20";
 
   src = fetchFromGitHub {
     repo = "proot";
     owner = "termux";
-    rev = "60485d2646c1e09105099772da4a20deda8d020d";
-    sha256 = "sha256-zHFPiL3ywZa8yzZa600BpoE+zuRipw2GNJrt3/Dy+/E=";
+    rev = "28baec5ac3e0f26c03cf2a4cdc094c9328bd5b20";
+    sha256 = "sha256-TMYkLmk+NnYcqJKF6RSOkN4S8AI5+HaNcgZZe/5E0vI=";
   };
 
   # ashmem.h is rather small, our needs are even smaller, so just define these:
