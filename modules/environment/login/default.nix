dff2d1eb9b40d88c523a62252d899219105c5517
Alex Martens <alex@thinglab.org>
2026-05-04 20:15:30 -0700

    proot-termux: unstable-2024-05-04 -> unstable-2026-02-20
    This contains a patch to fix the TCSETS2 syscall used by tcgetattr.

--- modules/environment/login/default.nix
+++ modules/environment/login/default.nix
@@ -87,8 +87,8 @@ in
       prootStatic =
         let
           crossCompiledPaths = {
-            aarch64-linux = "/nix/store/7qd99m1w65x2vgqg453nd70y60sm3kay-proot-termux-static-aarch64-unknown-linux-android-unstable-2024-05-04";
-            x86_64-linux = "/nix/store/pakj3svvw84rhkzdc6211yhc2cgvc21f-proot-termux-static-x86_64-unknown-linux-android-unstable-2024-05-04";
+            aarch64-linux = "/nix/store/dvf2ck9bkw7yyrlkjk87xz1anaxsgrd6-proot-termux-static-aarch64-unknown-linux-android-unstable-2026-02-20";
+            x86_64-linux = "/nix/store/w7fksni07fhss7hzkyf4knn5171prj88-proot-termux-static-x86_64-unknown-linux-android-unstable-2026-02-20";
           };
         in
         "${crossCompiledPaths.${targetSystem}}";
