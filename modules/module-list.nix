2ca23f7d8fbd6d4e99dab1fb50ddc1c1f74ddb24
Alex Martens <alex@thinglab.org>
2026-05-09 11:03:43 -0700

    modules: add programs.ssh

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -21,6 +21,7 @@
   ./environment/shell.nix
   ./home-manager.nix
   ./nixpkgs/options.nix
+  ./programs/ssh.nix
   ./terminal.nix
   ./time.nix
   ./upgrade.nix
---
+++ modules/programs/ssh.nix
@@ -0,0 +1,170 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.ssh;
+
+  knownHosts = builtins.attrValues cfg.knownHosts;
+
+  knownHostsText =
+    (lib.flip (lib.concatMapStringsSep "\n") knownHosts (
+      h:
+        assert h.hostNames != [ ];
+        lib.optionalString h.certAuthority "@cert-authority "
+        + builtins.concatStringsSep "," h.hostNames
+        + " "
+        + (if h.publicKey != null then h.publicKey else builtins.readFile h.publicKeyFile)
+    ))
+    + "\n";
+
+  knownHostsFiles = [
+    "/etc/ssh/ssh_known_hosts"
+  ] ++ map pkgs.copyPathToStore cfg.knownHostsFiles;
+in
+
+{
+
+  ###### interface
+
+  options.programs.ssh = {
+
+    knownHosts = lib.mkOption {
+      default = { };
+      type = lib.types.attrsOf (
+        lib.types.submodule (
+          { name
+          , config
+          , options
+          , ...
+          }:
+          {
+            options = {
+              certAuthority = lib.mkOption {
+                type = lib.types.bool;
+                default = false;
+                description = ''
+                  This public key is an SSH certificate authority, rather than an
+                  individual host's key.
+                '';
+              };
+              hostNames = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ name ] ++ config.extraHostNames;
+                defaultText = lib.literalExpression "[ ${name} ] ++ config.${options.extraHostNames}";
+                description = ''
+                  A list of host names and/or IP numbers used for accessing
+                  the host's ssh service. This list includes the name of the
+                  containing `knownHosts` attribute by default
+                  for convenience. If you wish to configure multiple host keys
+                  for the same host use multiple `knownHosts`
+                  entries with different attribute names and the same
+                  `hostNames` list.
+                '';
+              };
+              extraHostNames = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ ];
+                description = ''
+                  A list of additional host names and/or IP numbers used for
+                  accessing the host's ssh service. This list is ignored if
+                  `hostNames` is set explicitly.
+                '';
+              };
+              publicKey = lib.mkOption {
+                default = null;
+                type = lib.types.nullOr lib.types.str;
+                example = "ecdsa-sha2-nistp521 AAAAE2VjZHN...UEPg==";
+                description = ''
+                  The public key data for the host. You can fetch a public key
+                  from a running SSH server with the {command}`ssh-keyscan`
+                  command. The public key should not include any host names, only
+                  the key type and the key itself.
+                '';
+              };
+              publicKeyFile = lib.mkOption {
+                default = null;
+                type = lib.types.nullOr lib.types.path;
+                description = ''
+                  The path to the public key file for the host. The public
+                  key file is read at build time and saved in the Nix store.
+                  You can fetch a public key file from a running SSH server
+                  with the {command}`ssh-keyscan` command. The content
+                  of the file should follow the same format as described for
+                  the `publicKey` option. Only a single key
+                  is supported. If a host has multiple keys, use
+                  {option}`programs.ssh.knownHostsFiles` instead.
+                '';
+              };
+            };
+          }
+        )
+      );
+      description = ''
+        The set of system-wide known SSH hosts. To make simple setups more
+        convenient the name of an attribute in this set is used as a host name
+        for the entry. This behaviour can be disabled by setting
+        `hostNames` explicitly. You can use
+        `extraHostNames` to add additional host names without
+        disabling this default.
+      '';
+      example = lib.literalExpression ''
+        {
+          myhost = {
+            extraHostNames = [ "myhost.mydomain.com" "10.10.1.4" ];
+            publicKeyFile = ./pubkeys/myhost_ssh_host_dsa_key.pub;
+          };
+          "myhost2.net".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIRuJ8p1Fi+m6WkHV0KWnRfpM1WxoW8XAS+XvsSKsTK";
+          "myhost2.net/dsa" = {
+            hostNames = [ "myhost2.net" ];
+            publicKeyFile = ./pubkeys/myhost2_ssh_host_dsa_key.pub;
+          };
+        }
+      '';
+    };
+
+    knownHostsFiles = lib.mkOption {
+      default = [ ];
+      type = with lib.types; listOf path;
+      description = ''
+        Files containing SSH host keys to set as global known hosts.
+        `/etc/ssh/ssh_known_hosts` (which is
+        generated by {option}`programs.ssh.knownHosts`) is
+        always included.
+      '';
+      example = lib.literalExpression ''
+        [
+          ./known_hosts
+          (writeText "github.keys" '''
+            github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+            github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+            github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+          ''')
+        ]
+      '';
+    };
+
+  };
+
+
+  ###### implementation
+
+  config = {
+
+    assertions =
+      lib.flip lib.mapAttrsToList cfg.knownHosts (
+        name: data: {
+          assertion =
+            (data.publicKey == null && data.publicKeyFile != null)
+            || (data.publicKey != null && data.publicKeyFile == null);
+          message = "knownHost ${name} must contain either a publicKey or publicKeyFile";
+        }
+      );
+
+    environment.etc = {
+      "ssh/ssh_known_hosts".text = knownHostsText;
+    } // lib.optionalAttrs (cfg.knownHosts != { } || cfg.knownHostsFiles != [ ]) {
+      "ssh/ssh_config".text = "GlobalKnownHostsFile ${builtins.concatStringsSep " " knownHostsFiles}\n";
+    };
+
+  };
+
+}
